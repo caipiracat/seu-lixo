7ae13e50a36c6449113597273deae46faaa72133
Tiago Rafael Godinho <tiagogodinho3@gmail.com>
2012-12-07 17:42:58 -0200

    Adicionando os dados no js

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,8 @@ class HomeController < ApplicationController
   layout :false
 
   def index
+    @data = Locale.limit(10).collect do |locale|
+      { name: locale.name, garbage: 0, garbage_per_capita: 0} # TODO: Colocar os dados calculados aqui
+    end
   end
 end
