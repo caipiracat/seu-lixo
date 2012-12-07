ef2e3300c0038f7a59e5fcfb89b3f16d34675e26
Tiago Rafael Godinho <tiagogodinho3@gmail.com>
2012-12-07 10:50:30 -0200

    Adicionando email e site aos atributos acessíveis em Locale

--- app/models/locale.rb
+++ app/models/locale.rb
@@ -1,5 +1,5 @@
 class Locale < ActiveRecord::Base
-  attr_accessible :name, :population, :state
+  attr_accessible :name, :population, :state, :email, :site
 
   has_many :garbages
   has_many :idhs
