31e25c5f4ff0d90a020140d7432b072638143f13
Tiago Rafael Godinho <tiagogodinho3@gmail.com>
2012-12-07 18:41:28 -0200

    Usando os dados do banco na homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,8 @@ class HomeController < ApplicationController
   layout :false
 
   def index
-    @data = Locale.limit(10).collect do |locale|
-      { name: locale.name, garbage: 0, garbage_per_capita: 0} # TODO: Colocar os dados calculados aqui
+    @data = Locale.includes(:idhs, :garbages).order('garbages.total desc').limit(10).collect do |locale|
+      { name: locale.name, garbage: locale.garbages.first.total.round(0), garbage_per_capita: ( ( locale.garbages.first.total.to_f / locale.population.to_f ) * 1000 ).round(2) } # TODO: Colocar os dados calculados aqui
     end
   end
 end
