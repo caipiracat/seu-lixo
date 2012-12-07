4199679a534df602849ea2766b5ddf1a642b2a2e
Tiago Rafael Godinho <tiagogodinho3@gmail.com>
2012-12-07 19:37:47 -0200

    Corrige um problema no heroku

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
 
   def index
     @data = Locale.includes(:idhs, :garbages).order('garbages.total desc').limit(10).collect do |locale|
-      { name: locale.name, garbage: (locale.garbages.first.total || 0).round(0), garbage_per_capita: ( ( (locale.garbages.first.total || 0).to_f / locale.population.to_f ) * 1000 ).round(2) } # TODO: Colocar os dados calculados aqui
+      { name: locale.name, garbage: (locale.garbages.first.try(:total) || 0).round(0), garbage_per_capita: ( ( (locale.garbages.first.try(:total) || 0).to_f / locale.population.try(:to_f) ) * 1000 ).round(2) } # TODO: Colocar os dados calculados aqui
     end
   end
 end
