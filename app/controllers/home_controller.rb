a3e5dc319b3194edc5b5de9f02fe91fdafabab88
Tiago Rafael Godinho <tiagogodinho3@gmail.com>
2012-12-07 23:59:25 -0200

    Inclusão dos gráficos na pagina cidades

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,5 @@ class HomeController < ApplicationController
   #layout :false
 
   def index
-    @data = Locale.includes(:idhs, :garbages).order('garbages.total desc').limit(10).collect do |locale|
-      { name: locale.name, garbage: (locale.garbages.first.try(:total) || 0).round(0), garbage_per_capita: ( ( (locale.garbages.first.try(:total) || 0).to_f / locale.population.try(:to_f) ) * 1000 ).round(2),idh:locale.idhs.first.idhm } # TODO: Colocar os dados calculados aqui
-    end
   end
 end
