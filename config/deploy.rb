51aae6641a406847729a7f0971216856b5fdf73e
Tiago Rafael Godinho <tiagogodinho3@gmail.com>
2012-12-08 05:33:42 -0200

    Atualização do capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,8 @@ set :deploy_to,   "/home/#{user}/rails_apps/#{application}"
 set :use_sudo,    false
 set :scm,         :git
 
+after 'deploy:update_code', 'deploy:symlink_shared'
+
 namespace :deploy do
   task :start do
     run "cd #{current_path} && bundle exec unicorn -c config/unicorn.rb -E #{rails_env} -D"
@@ -23,10 +25,15 @@ namespace :deploy do
     run "kill -s USR2 `cat #{current_path}/tmp/pids/unicorn.pid`"
   end
 
+  desc 'Symlink shared configs and folders on each release.'
+    task :symlink_shared do
+      run "ln -s #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    end
+
   namespace :db do
     desc 'Populates the Production Database'
     task :seed do
-      run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{stage}"
+      run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=production"
     end
   end
 end
