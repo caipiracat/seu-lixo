0738f4b07ca907c7e2720518b9a041f8e12ec11e
Tiago Rafael Godinho <tiagogodinho3@gmail.com>
2012-12-08 01:16:21 -0200

    Tentando corrigir um problema no Heroku

--- app/controllers/locales_controller.rb
+++ app/controllers/locales_controller.rb
@@ -4,7 +4,7 @@ class LocalesController < ApplicationController
   def index
     @locales = Locale.all
     
-    @data = Locale.includes(:idhs, :garbages).order('garbages.total desc').limit(10).collect do |locale|
+    @data = Locale.includes(:idhs, :garbages).order('garbages.total DESC').limit(10).collect do |locale|
       { name: locale.name, garbage: (locale.garbages.first.try(:total) || 0).round(0), garbage_per_capita: ( ( (locale.garbages.first.try(:total) || 0).to_f / locale.population.try(:to_f) ) * 1000 ).round(2) } # TODO: Colocar os dados calculados aqui
     end
 
