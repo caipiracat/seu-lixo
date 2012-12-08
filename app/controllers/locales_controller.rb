13be3e3b21d776b690fda404b81841f092d2e125
Tiago Rafael Godinho <tiagogodinho3@gmail.com>
2012-12-08 00:35:10 -0200

    Correção de um problema na página "Cidades"

--- app/controllers/locales_controller.rb
+++ app/controllers/locales_controller.rb
@@ -5,7 +5,7 @@ class LocalesController < ApplicationController
     @locales = Locale.all
     
     @data = Locale.includes(:idhs, :garbages).order('garbages.total desc').limit(10).collect do |locale|
-      { name: locale.name, garbage: (locale.garbages.first.try(:total) || 0).round(0), garbage_per_capita: ( ( (locale.garbages.first.try(:total) || 0).to_f / locale.population.try(:to_f) ) * 1000 ).round(2),idh:locale.idhs.first.idhm } # TODO: Colocar os dados calculados aqui
+      { name: locale.name, garbage: (locale.garbages.first.try(:total) || 0).round(0), garbage_per_capita: ( ( (locale.garbages.first.try(:total) || 0).to_f / locale.population.try(:to_f) ) * 1000 ).round(2) } # TODO: Colocar os dados calculados aqui
     end
 
     respond_to do |format|
